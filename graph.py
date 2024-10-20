63ca030a0e4b07675a4d63648f4f9c0bc0f86481
mesutdmn <mesutduman@outlook.com>
2024-10-20 23:45:01 +0300

    live

--- graph.py
+++ graph.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 from langgraph.graph import StateGraph, END
 from langchain_openai import ChatOpenAI
 from pydantic import BaseModel, Field
