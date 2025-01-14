9e7d023d788d8f49d189bf445375765d3e9debc5
surajpaigudehrb <44489994+surajpaigudehrb@users.noreply.github.com>
2025-01-14 19:23:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,11 @@ if "messages" not in st.session_state:
 
 with st.sidebar:
     st.info(" * This app uses the GROQ API to generate text, please provide your API key."
-            "\n\n * This app uses the 'gpt-4o-mini-2024-07-18' model. Cost effective and efficient."
-            "\n\n * If you don't have an API key, you can get one [here](https://platform.openai.com/signup)."
-            "\n\n * You can also find the source code for this app [here](https://github.com/mesutdmn/Autonomous-Multi-Agent-Systems-with-CrewAI-Essay-Writer)"
+            "\n\n * This app uses the 'mixtral-8x7b-32768' model. Cost effective and efficient."
             "\n\n * App keys are not stored or saved in any way."
             "\n\n * Writing essay may take some time, please be patient. Approximately 1-2 minutes."
             "\n\n * If you like this app, consider buying me a coffee ☕")
-    openai_key_input = st.text_input("GROQ API Key", type="password")
+    openai_key_input = st.text_input("GROQ API Key", type="password",value="1234")
     developer_mode = st.checkbox("I don't have key, use developer's money 😓", value=False)
     openai_key = openai_key_input or (st.secrets["GROQ_API_KEY"] if developer_mode else "")
 
